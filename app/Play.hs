82a5340ee1b36cb8c86a876354d513eb1e1d3594
Chance Onyiorah <chanceonyiorah@gmail.com>
2022-12-18 22:14:40 -0500

    game play and minimax algorithm

--- app/Play.hs
+++ app/Play.hs
@@ -1,21 +1,24 @@
 module Main where
 import           Data.List
 import qualified Data.Vector                   as V
+import           Mancala
+import           Minimax
 import           System.Exit
 import           System.IO
 import           Text.Printf
 
-data Player = Computer | Player2
-    deriving (Eq, Show)
+{-
+Starting game board
 
-data Board = Board (V.Vector Int)
-    deriving (Show)
+    A  B  C  D  E  F
+P   6  6  6  6  6  6
+0                    0
+    6  6  6  6  6  6  C
+    L  K  J  I  H  G
+-}
 
-data MancalaGameState = MancalaGameState Board Player Player
-    deriving Show
-
-initialBoard = Board $ V.fromList (take 14 (cycle [6, 6, 6, 6, 6, 6, 0]))
-
+-- translate letters into numeric spaces
+--valid computer moves
 getComputerMove :: String -> Int
 getComputerMove "L" = 0
 getComputerMove "K" = 1
@@ -23,11 +26,11 @@ getComputerMove "J" = 2
 getComputerMove "I" = 3
 getComputerMove "H" = 4
 getComputerMove "G" = 5
---quit
-getComputerMove "q" = 13
-getComputerMove "Q" = 13 
-getComputerMove _ = 14 --invalid
+getComputerMove "q" = 13 --quit
+getComputerMove "Q" = 13 --quit
+getComputerMove _ = 14 --invalid input
 
+--valid player moves
 getPlayerMove :: String -> Int 
 getPlayerMove "A" = 12
 getPlayerMove "B" = 11
@@ -35,11 +38,11 @@ getPlayerMove "C" = 10
 getPlayerMove "D" = 9
 getPlayerMove "E" = 8
 getPlayerMove "F" = 7
---quit
-getPlayerMove "q" = 13
-getPlayerMove "Q" = 13
-getPlayerMove _ = 14 --invalid
+getPlayerMove "q" = 13 --quit
+getPlayerMove "Q" = 13 --quit
+getPlayerMove _ = 14 --invalid inpuy
 
+-- translate numeric spaces into letters 
 getComputerLetter :: Int -> String
 getComputerLetter 0 = "L"
 getComputerLetter 1 = "K"
@@ -49,65 +52,106 @@ getComputerLetter 4 = "H"
 getComputerLetter 5 = "G"
 getComputerLetter _ = error "Invalid move"
 
-getMove :: Player -> IO Int
-getMove p = do
+--input move, check if move is valid
+getMove :: Player -> Board -> IO Int
+getMove p (Board b) = do
     str <- getLine
-    let num = if p == Computer then getComputerMove str else getPlayerMove str
-    case num of
-        14 -> do
+    let move = if p == Computer then getComputerMove str else getPlayerMove str
+    if (move == 14 || ((b V.! move) == 0 && move /= 13)) --invalid letter or hole that is empty
+        then do
             putStr "Invalid Move. Try again: "
             hFlush stdout
-            getMove p
-        13 -> do
-            putStrLn "Quitting."
-            exitWith ExitSuccess
-        num -> return num
-
+            getMove p (Board b)
+        else do
+            if move == 13 --quit key
+            then do
+                putStrLn "Quitting."
+                exitWith ExitSuccess
+            else return move
+                
+--identify who's turn it is
 printPlayer :: Player -> IO ()
-printPlayer Computer = putStrLn "Computer's side: "
-printPlayer Player2 = putStrLn "Your side: "
+printPlayer Computer = putStrLn "Computer: "
+printPlayer Player2 = putStrLn "You: "
 
-letterRow :: [Char] -> String
-letterRow chars = intercalate "  " $ map (\x -> [x]) chars
-
-stringRow :: Board -> [Int] -> IO String
-stringRow (Board b) xs = do
+--print marbles in each hole
+printMarbles :: Board -> [Int] -> IO String
+printMarbles (Board b) xs = do
     lineStr <-
         return
             (foldl (\str n -> str ++ (printf "%3d" n)) "" (map (\i -> b V.! i) xs)
             )
     return lineStr
 
+--print hole letters on top along with marbles
 printTopRow :: Board -> IO ()
 printTopRow b = do
-    str <- stringRow b [12, 11 .. 7]
-    putStrLn $ "        " ++ "   " ++ letterRow ['A' .. 'F']
+    str <- printMarbles b [12, 11 .. 7]
+    putStrLn $ "        " ++ "   " ++ "A  B  C  D  E  F"
     putStrLn $ "        " ++ "P" ++ str
 
+--print hole letters on bottom along with marbles
 printBottomRow :: Board -> IO ()
 printBottomRow b = do
-    str <- stringRow b [0 .. 5]
+    str <- printMarbles b [0 .. 5]
     putStrLn $ "        " ++ " " ++ str ++ "  C"
-    putStrLn $ "        " ++ "   " ++ letterRow (reverse ['G' .. 'L'])
+    putStrLn $ "        " ++ "   " ++ "L  K  J  I  H  G"
 
-
-printMarbles :: Board -> IO ()
-printMarbles (Board b) =
+--print both stores
+printStores :: Board -> IO ()
+printStores (Board b) =
     putStrLn $ "        " ++ (show $ b V.! 13) ++ (replicate 20 ' ') ++ (show $ b V.! 6)
 
+--print board
 printBoard :: Board -> IO ()
 printBoard b = do
     printTopRow b
-    printMarbles b
+    printStores b
     printBottomRow b
 
+--print board and get game input
 printGameState :: MancalaGameState -> IO ()
 printGameState (MancalaGameState b p _) = do
     printPlayer p
     printBoard b
 
+applyMove gs move = return (distributeMarbles gs move) >>= playGame
+
+humanMoveGS (MancalaGameState board player _) = do
+    m <- getMove player board
+    putStrLn ""
+    return m
+
+makeMoveGS :: MancalaGameState -> IO Int
+makeMoveGS gs = do
+    let (score, move) = minimax gs 0 8 (-10000) 10000
+    case move of
+        (Just x) -> do
+            printf "Computer move: '%s'.\n\n"
+                   (getComputerLetter x)
+            return x
+        Nothing -> error "Invalid move: Nothing"
+
+playGame :: MancalaGameState -> IO()
+playGame (MancalaGameState board computer player) | rowEmpty board Computer || rowEmpty board Player2 =
+    let winString = case (evaluate (MancalaGameState board computer player)) of
+            x | x > 0 -> "Winner is " ++ (show player)
+            x | x < 0 -> "Winner is " ++ (show other)
+            0         -> "Tie."
+            where other | player == Computer = Player2
+                            | otherwise = Computer
+    in  (putStrLn $ "Game over. " ++ winString) >> printGameState (MancalaGameState board computer player)
+playGame (MancalaGameState board Computer Computer) =
+    printGameState (MancalaGameState board Computer Computer) >> putStrLn "Computer's turn" >> makeMoveGS (MancalaGameState board Computer Computer) >>= \m ->
+        applyMove (MancalaGameState board Computer Computer) m
+playGame (MancalaGameState board Player2 x) =
+    printGameState (MancalaGameState board Player2 x)
+        >>  putStr "Enter move: "
+        >>  hFlush stdout
+        >>  humanMoveGS (MancalaGameState board Player2 x)
+        >>= \m -> applyMove (MancalaGameState board Player2 x) m
 
 startGameState = MancalaGameState initialBoard Computer Computer
 main = do
     startGS <- return startGameState
-    printGameState startGS
+    playGame startGS
--- src/Minimax.hs
+++ src/Minimax.hs
@@ -1,60 +1,55 @@
-module Minimax where
-import           Data.List
-import           Data.Ord
-import           Debug.Trace
-import qualified Data.Vector   as V
+module Minimax
+where
+import Debug.Trace
+import Data.List
+import Data.Ord
 
 class GameState a where
-    pointsLeft    :: a -> Int
+    evaluate    :: a -> Int
     gameOver    :: a -> Bool
     possibleMoves    :: a -> [Int]
-    startGameState    :: a -> Int -> a
+    makeSuccessor    :: a -> Int -> a
+    isMaximizing    :: a -> Bool
 
-data Player = Computer | Player
-    deriving (Eq, Show)
+{-
+Test:
+> gs = MancalaGameState initialBoard Computer Computer
+> alphabeta gs False 0 8 (-1000) 1000
+-}
 
-data Board = Board (V.Vector Int)
-    deriving (Show)
+minimax   :: (GameState a) => a -> Int -> Int -> Int -> Int -> (Int, Maybe Int)
+minimax gs _ _ _ _ | gameOver gs = (evaluate gs, Nothing)
+minimax gs depth depthlimit _ _ | depth == depthlimit = (evaluate gs, Nothing)
+minimax gs depth depthlimit alpha beta =
+    alphabetafold successors alpha beta (-1)
+    where   successors = possibleMoves gs
+            alphabetafold [] a _ bestChild = (a, Just bestChild)
+            alphabetafold (x:xs) a b bestChild =
+                let child = makeSuccessor gs x
+                    newAlpha = (if (isMaximizing child) then alphabetamax else alphabetamin) child (depth+1) depthlimit a b in
+                if (newAlpha >= b)
+                then (newAlpha, Just x)
+                else alphabetafold xs (max a newAlpha) b (if newAlpha > a then x else bestChild)
 
-data MancalaGameState = MancalaGameState Board Player Player
+alphabetamax    :: (GameState a) => a -> Int -> Int -> Int -> Int -> Int
+alphabetamax gs _ _ _ _ | gameOver gs = evaluate gs
+alphabetamax gs depth depthlimit _ _ | depth == depthlimit = evaluate gs
+alphabetamax gs depth depthlimit alpha beta =
+    alphabetaHelper gs successors alpha beta depth depthlimit
+    where   successors = possibleMoves gs
 
-initialBoard = Board $ V.fromList (take 14 (cycle [6, 6, 6, 6, 6, 6, 0]))
-    
-instance GameState MancalaGameState where
-    pointsLeft _ = 80
-    gameOver _ = False
-    possibleMoves _ = [1,3,5]
-    startGameState _ _= MancalaGameState initialBoard Computer Computer
+alphabetamin    :: (GameState a) => a -> Int -> Int -> Int -> Int -> Int
+alphabetamin gs _ _ _ _ | gameOver gs = evaluate gs
+alphabetamin gs depth depthlimit _ _ | depth == depthlimit = evaluate gs
+alphabetamin gs depth depthlimit alpha beta =
+    alphabetaHelper gs successors alpha beta depth depthlimit
+    where   successors = possibleMoves gs
 
-
---Test:
--- ghc -threaded -rtsopts -eventlog --make -main-is Minimax  Minimax.hs -package vector
--- time ./Minimax +RTS -ls -s
--- time ./Minimax +RTS -N2 -ls -s
-main :: IO()
-main = do
-    let gs = MancalaGameState initialBoard Computer Computer
-    print (minimax gs False 0 8)
-    --(80,Just 5)
-
-
-minimax :: (GameState a) => a -> Bool -> Int -> Int -> (Int, Maybe Int)
-minimax gs _ depth depthlimit 
-    | depth == depthlimit || gameOver gs = (pointsLeft gs, Nothing)
-minimax gs minimize depth depthlimit =
-    let
-        minOrMax   = (if minimize then minimumBy else maximumBy) (comparing fst)
-        successors = possibleMoves gs
-        scores     = map fst $ map
-            (\succ ->
-                (minimax (startGameState gs succ)
-                         (not minimize)
-                         (depth + 1)
-                         depthlimit
-                )
-            )
-            successors
-        wrappedSuccessors = map Just successors
-        scoreSuccPairs    = zip scores wrappedSuccessors
-    in
-        minOrMax scoreSuccPairs
+alphabetaHelper :: (GameState a) => a -> [Int] -> Int -> Int -> Int -> Int -> Int
+alphabetaHelper _ [] _ b _ _= b
+alphabetaHelper gs (x:xs) a b depth depthlimit =
+    let child = makeSuccessor gs x
+        newBeta = (if (isMaximizing child) then alphabetamax else alphabetamin) child (depth+1) depthlimit a b in
+    if (newBeta <= a)
+        then newBeta
+        else alphabetaHelper gs xs a (min b newBeta) depth depthlimit
